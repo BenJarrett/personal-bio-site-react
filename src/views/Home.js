cb7bc40c377e0d9fd132fa9110931adc753547a2
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-05-24 20:59:45 -0500

    Set up files done

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../App/components/NavBar';
+
+function Home() {
+  return (
+    <>
+    <NavBar>
+    </NavBar>
+    </>
+  );
+}
+
+export default Home;
