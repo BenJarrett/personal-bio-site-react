6acccb127128a001ce20cfcf8fd648031d3135db
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-05-26 14:41:48 -0500

    Added form

--- src/App/components/ProjectCard.js
+++ src/App/components/ProjectCard.js
@@ -1,67 +1,75 @@
 import React, { useState } from 'react';
-
+// import { useHistory } from 'react-router-dom';
 import {
-  Button,
   Card,
-  CardText,
-  CardTitle
+  Button,
+  CardTitle,
+  CardText
 } from 'reactstrap';
 import PropTypes from 'prop-types';
+import { deleteProject } from '../../helpers/data/projectsData';
+import ProjectsForm from './ProjectForm';
 
-// import { deletePlayer } from '../../helpers/data/playerData';
-// import PlayerForm from './PlayerForm';
-
-const ProjectCard = ({
-  // firebaseKey,
+const ProjectsCard = ({
+  firebaseKey,
   projects,
-  // setProjects,
-  // admin
-
+  setProjects
 }) => {
   const [editing, setEditing] = useState(false);
+  // const history = useHistory();
 
   const handleClick = (type) => {
     switch (type) {
-      // case 'delete':
-      //   deletePlayer(firebaseKey)
-      //     .then(setPlayers);
-      //   break;
+      case 'delete':
+        deleteProject(firebaseKey)
+          .then((projectArray) => setProjects(projectArray));
+        break;
       case 'edit':
         setEditing((prevState) => !prevState);
         break;
       default:
-        console.warn('nothing selected');
+        console.warn('nope');
     }
-    // console.warn(firebaseKey);
   };
-
   return (
     projects.map((projectInfo) => (
-  <Card key={projectInfo.firebaseKey}>
-      <CardTitle tag="h5">{projectInfo.title}</CardTitle>
-      <CardText>githubUrl: {projectInfo.githubUrl}</CardText>
-      <CardText>screenshot: {projectInfo.screenshot}</CardText>
-      <CardText>technologiesUsed: {projectInfo.technologiesUsed}</CardText>
-      <CardText>url: {projectInfo.url}</CardText>
-      <CardText></CardText>
-      <img style={{ width: '14rem', alignSelf: 'center', height: '18rem' }} src={projectInfo.screenshot} className="photo" alt="Card image cap" />
-      <Button style={{ backgroundColor: '#D8E1FF', color: '#000000', margin: '1rem' }} onClick={() => handleClick('delete')}></Button>
-      <Button style={{ backgroundColor: '#D8E1FF', color: '#000000', margin: '1rem' }}onClick={() => handleClick('edit')}>
-        {editing ? 'Close Form' : 'Edit Player'}
-      </Button>
-    </Card>
+        <Card
+        key={projectInfo.firebaseKey}
+        style={{
+          width: '20rem',
+          flex: 'initial',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+        }} body>
+         <CardTitle tag="h5">Project Name: {projectInfo.title}</CardTitle>
+         <CardText>url: {projectInfo.url}</CardText>
+         <CardText>Github Url: {projectInfo.githubUrl}</CardText>
+         <CardText>Technologies Used: {projectInfo.technologiesUsed}</CardText>
+         <img style={{ width: '16rem' }} src={projectInfo.screenshot} className="photo" alt="Card image cap" />
+         <Button style={{ backgroundColor: '#ffa64d' }} onClick={() => handleClick('delete')}>Delete Project</Button>
+         <Button style={{ backgroundColor: '#004d1a' }} onClick={() => handleClick('edit')}>
+           {editing ? 'Close Form' : 'Edit Project'}
+           </Button>
+         {
+         editing && <ProjectsForm
+         formTitle='Edit Project'
+         setProjects={setProjects}
+         firebaseKey={firebaseKey}
+         githubUrl={projectInfo.githubUrl}
+         screenshot={projectInfo.screenshot}
+         technologiesUsed={projectInfo.technologiesUsed}
+         title={projectInfo.title}
+         url={projectInfo.url}
+         />
+         }
+         </Card>
     ))
   );
 };
 
-ProjectCard.propTypes = {
+ProjectsCard.propTypes = {
   firebaseKey: PropTypes.string,
-  githubUrl: PropTypes.string,
-  screenshot: PropTypes.string,
-  technologiesUsed: PropTypes.string,
-  title: PropTypes.string,
-  url: PropTypes.string,
+  projects: PropTypes.array,
   setProjects: PropTypes.func
 };
-
-export default ProjectCard;
+export default ProjectsCard;
